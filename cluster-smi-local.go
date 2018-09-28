699f960dd7fdc403f80eb26aaf1143371f165168
minostauros <tigerdream1@gmail.com>
2018-09-28 13:26:35 +0900

    make cluster-smi-local same with cluster-smi

--- cluster-smi-local.go
+++ cluster-smi-local.go
@@ -19,9 +19,12 @@ func main() {
 	// load ports and ip-address
 	cfg := LoadConfig()
 
+	showJustOnce := flag.Bool("once", false, "Show result just once")
 	showTimePtr := flag.Bool("t", false, "show time of events")
 	showExtendedPtr := flag.Bool("e", false, "extended view")
 	showProcessesPtr := flag.Bool("p", false, "verbose process information")
+	showContainerPtr := flag.Bool("c", false, "show Docker container")
+	showShortPtr := flag.Bool("s", false, "show result w/o command, gpu mem, and runtime")
 	nodeRegex := flag.String("n", ".", "match node-names with regex for display information "+
 		"(if not specified, all nodes will be shown)")
 	usernameFilter := flag.String("u", "", "show all information only for specific user")
@@ -48,7 +51,18 @@ func main() {
 		}
 
 		clus.FilterNodes(*nodeRegex)
-		clus.Print(*showProcessesPtr, *showTimePtr, cfg.Timeout, *useColor, *showExtendedPtr)
+		if *showShortPtr {
+			*showContainerPtr = true
+		}
+		if *showProcessesPtr {
+			*showContainerPtr = true
+			*showShortPtr = true
+		}
+		clus.Print(*showProcessesPtr, *showTimePtr, cfg.Timeout, *useColor, *showExtendedPtr, *showShortPtr, *showContainerPtr)
+		
+		if *showJustOnce {
+			break
+		}
 		time.Sleep(time.Duration(cfg.Tick) * time.Second)
 	}
 
